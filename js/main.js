bbb133684f2b17f852c231593c6fdcb0f8235621
Georgi Kolarov <gkolarov@mw-medianetworks.eu>
2019-02-04 18:42:26 +0200

    ready calendar

--- js/main.js
+++ js/main.js
@@ -47,3 +47,20 @@ $('.grid').masonry({
     columnWidth: 320,
     isFitWidth: true
 });
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1;
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+    dd = '0' + dd;
+}
+
+if (mm < 10) {
+    mm = '0' + mm;
+}
+
+today = mm + '/' + dd + '/' + yyyy;
+
+$('.todayDate').text(today);
